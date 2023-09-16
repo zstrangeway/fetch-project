a46e36daf145a5a1009db7acff5b81c0c497a15f
Zac Strangeway <zac.strangeway@gmail.com>
2023-09-16 13:19:34 -0500

    feat(FTH-03) add fetch api service

---
+++ src/services/fetch-api-service.ts
@@ -0,0 +1,94 @@
+import axios from 'axios';
+import * as LoggingService from './logging-service';
+import { LogLevel } from './logging-service';
+import { SearchInputs } from '../components/SearchForm';
+
+const BASE_URL = import.meta.env.VITE_APP_API_URL;
+const SERVICE_NAME = 'FetchApiService';
+
+const httpClient = axios.create({
+  withCredentials: true,
+  baseURL: BASE_URL,
+});
+
+export interface LoginParams {
+  name: string;
+  email: string;
+}
+
+export const login = async (params: LoginParams): Promise<void> => {
+  try {
+    await httpClient.post('/auth/login', params);
+  } catch (error) {
+    LoggingService.log(LogLevel.Debug, `${SERVICE_NAME}.login()`, error);
+    throw error;
+  }
+};
+
+export const logout = async (): Promise<void> => {
+  try {
+    await httpClient.post('/auth/logout');
+  } catch (error) {
+    LoggingService.log(LogLevel.Debug, `${SERVICE_NAME}.logout()`, error);
+    throw error;
+  }
+};
+
+export const listDogBreeds = async (): Promise<string[]> => {
+  try {
+    const response = await httpClient.get<string[]>('/dogs/breeds');
+    return response.data;
+  } catch (error) {
+    LoggingService.log(LogLevel.Debug, `${SERVICE_NAME}.listDogBreeds()`, error);
+    throw error;
+  }
+};
+
+interface SearchDogsResponse {
+  next: string;
+  resultIds: string[];
+  total: number;
+}
+
+interface Dog {
+  id: string
+  img: string
+  name: string
+  age: number
+  zip_code: string
+  breed: string
+}
+
+export const searchDogs = async (params?: SearchInputs): Promise<SearchDogsResponse> => {
+  try {
+    const response = await httpClient.get<SearchDogsResponse>('/dogs/search', { params });
+    return response.data;
+  } catch (error) {
+    LoggingService.log(LogLevel.Debug, `${SERVICE_NAME}.searchDogs()`, error);
+    throw error;
+  }
+};
+
+export const getDogs = async (params: string[]): Promise<Dog[]> => {
+  try {
+    const response = await httpClient.post<Dog[]>('/dogs', params);
+    return response.data;
+  } catch (error) {
+    LoggingService.log(LogLevel.Debug, `${SERVICE_NAME}.getDogs()`, error);
+    throw error;
+  }
+};
+
+interface MatchDogsResponse {
+  match: string;
+}
+
+export const matchDogs = async (params: string[]): Promise<string> => {
+  try {
+    const response = await httpClient.post<MatchDogsResponse>('/dogs/match', params);
+    return response.data.match;
+  } catch (error) {
+    LoggingService.log(LogLevel.Debug, `${SERVICE_NAME}.matchDogs()`, error);
+    throw error;
+  }
+};
